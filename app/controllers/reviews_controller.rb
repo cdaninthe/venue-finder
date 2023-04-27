a20cc6b7725533ddc5de74c951971557679de252
Cecilia Daninthe <cecilia.daninthe@gmail.com>
2023-04-27 18:42:50 -0400

    routes and controllers

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,40 @@
+class ReviewsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  
+    def index
+        render json: Review.all, status: :ok
+    end
+
+    def create
+        review = @current_user.reviews.create!(review_params)
+        render json: review, status: :created
+    end
+
+    def update
+        review = Review.find_by(id: params[:id])
+        review.update!(review_params)
+        render json: review, status: :accepted
+    end
+
+    def destroy
+        review = Review.find_by(id: params[:id])
+        review.destroy
+        head :no_content
+    end
+
+    private
+
+    def review_params
+        params.permit(:title, :comment, :user_id, :space_id)
+    end
+
+    def render_not_found_response
+        render json: {errors: ["Review was not found."]}, status: not_found 
+    end
+    
+    def render_unprocessable_entity_response(invalid)
+        render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+    end
+
+end
