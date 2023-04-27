8a5c47311bbfc32d15cd22ab40add16bde9de41e
Cecilia Daninthe <cecilia.daninthe@gmail.com>
2023-04-26 21:16:53 -0400

    updated seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,29 +14,29 @@ User.create(username: "Jane", password: "password")
 User.create(username: "Mary", password: "password")
 
 
-# CREATE SPACES
-Space.create(
+# CREATE VENUES
+Venue.create(
     name: "Garden center", 
     description: Faker::Lorem.paragraph_by_chars(number: 256, supplemental: false), 
     city: "Yonkers",
-    state: "New York"
+    state: "New York",
     image_url: 'https://images.unsplash.com/photo-1550948390-6eb7fa773072?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1748&q=80',
     price: 50, 
     user_id: User.first.id
 )
 
-Space.create(
+Venue.create(
     name: "Rooftop Terrace", 
     description: Faker::Lorem.paragraph_by_chars(number: 256, supplemental: false), 
     city: "New York",
-    state: "New York"
-    image_url: 'https://images.unsplash.com/photo-1493246318656-5bfd4cfb29b8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
+    state: "New York",
+    image_url: 'https://images.unsplash.com/photo-1493246318656-5bfd4cfb29b8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
     price: 150, 
     user_id: User.first.id
 )
 
 # # CREATE REVIEWS
-Space.all.each do |space|
+Venue.all.each do |space|
     2.times do
         space.reviews.create(
             title: Faker::Quote.singular_siegler,
